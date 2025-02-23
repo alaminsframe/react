b5eca9a280ecfdc99c42e75bf42330d813b123ea
alaminsframe <helloalaminsheikh@gmail.com>
2025-02-24 01:58:39 +0600

    first component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Banner from "./Banner"
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-slate-700">
+        <Banner />
+      </div>
+    </>
+  )
+}
+
+export default App
