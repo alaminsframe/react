24e472c96d3d589de92de202feaaee92b5fc6a9c
alaminsframe <helloalaminsheikh@gmail.com>
2025-04-27 14:02:45 +0600

    task-three

--- src/CheckInput.jsx
+++ src/CheckInput.jsx
@@ -6,7 +6,7 @@ const CheckInput = () => {
         type="checkbox"
         className="h-7 w-7 appearance-none border-2 rounded checked:bg-blue-400"
        />
-       <span className="font-bold text-sm md:text-base">Show Premium Only</span>
+       <span className="font-bold text-sm md:text-base">Show Premium Only task-three</span>
     </>
   )
 }
