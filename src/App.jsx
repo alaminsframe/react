503d8413f17a6b94b4d806b24294c1a44171bb93
alaminsframe <helloalaminsheikh@gmail.com>
2025-05-01 00:57:14 +0600

    wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from 'react'
 import Banner from "./Banner"
 import Exercise from "./Exercise/Exercise"
 import User from './Exercise/User';
+import Wrapper from './Exercise/Wrapper';
 function App() {
 const [student, setStudent] = useState();
 
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-slate-700 gap-2">
       <div>
-        <User name='jahan' />
+        <Wrapper>
+          <h1 className='text-2xl text-white'>Hello world</h1>
+          <p className='text-lg text-white'>This is a simple react app</p>
+        </Wrapper>
+
+        {/* <User name='jahan' />
         {student && <Exercise name= {student} /> }
         <button onClick={()=>setStudent('world hello')}>update name</button>
-        {/* <Banner /> */}
+        <Banner /> */}
       </div>
       
     </div>
---
+++ src/Exercise/Wrapper.jsx
@@ -0,0 +1,11 @@
+
+
+const Wrapper = ({children}) => {
+  return (
+    <div className="p-5 bg-slate-800 text-white rounded-lg">
+      {children}
+    </div>
+  )
+}
+
+export default Wrapper
