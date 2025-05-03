c9a066e9383f2875958cc49adc7540c2cac35a4e
alaminsframe <helloalaminsheikh@gmail.com>
2025-05-04 01:09:39 +0600

    obj state changing

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 const [student, setStudent] = useState();
 
   return (
-    <div className="h-screen flex flex-col justify-center items-center bg-slate-700 gap-2">
+    <div className="h-screen flex flex-col justify-center items-center text-white bg-slate-700 gap-2">
       <>
         <UserList />
         {/* <TextIput />
--- src/Exercise/UserList.jsx
+++ src/Exercise/UserList.jsx
@@ -3,13 +3,38 @@ import {useState} from 'react'
 const UserList = () => {
     const [users, setUsers] = useState([{
         id: crypto.randomUUID(), 
-        name: 'John',
+        name: 'John Doe',
         address: 'New York',
 
     }])
+
+    const changeUser = (id, name) => {
+        const newUsers = [...users];
+        const foundUser = newUsers.find( (user) => user.id === id );
+        if (foundUser) {
+            foundUser.name = name;
+        } 
+        setUsers(newUsers);
+    }
     return (
         <div>
-            
+            <ul>
+                {
+                    users.map((user) =>(
+                        <li key={user.id} className='flex gap-4 items-center'>
+                            <p className='text-2xl'>
+                                {user.name}
+                            </p>
+                            <button
+                                onClick = {() => changeUser(user.id, 'Random')}
+                                className='border text-white px-1.5 py-1 rounded-md'
+                            >
+                                Change User
+                            </button>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
