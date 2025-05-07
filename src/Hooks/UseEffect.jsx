3c6c5a9615ad47ac3a33f28c014d7a0238cfe795
alaminsframe <helloalaminsheikh@gmail.com>
2025-05-07 22:18:59 +0600

    useeffect

--- src/Hooks/UseEffect.jsx
+++ src/Hooks/UseEffect.jsx
@@ -2,14 +2,21 @@ import {useState, useEffect} from 'react'
 
 const UseEffect = () => {
     let [count, setCount] = useState(0);
+    const [name, setName] = useState('hello');
+    // useEffect(()=>{
+    //     setTimeout(() => {
+    //         setCount(++count)
+    //     }, 1000)
+    // })
     useEffect(()=>{
         setTimeout(() => {
             setCount(++count)
         }, 1000)
-    })
+    }, [name])
   return (
     <div>
       <p>I've rendered {count} times.</p>
+      <button onClick={()=>setName('hi')} className='border p-1'>change</button>
     </div>
   )
 }
