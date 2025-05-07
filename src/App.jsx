88b89d8bed26a0af8aa92ea56eba5d25c4b543e4
alaminsframe <helloalaminsheikh@gmail.com>
2025-05-07 22:43:54 +0600

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const [student, setStudent] = useState();
     <div className="h-screen flex flex-col justify-center items-center text-white bg-slate-700 gap-2">
       <>
         <UseRef />
-        <UseEffect />
+        {/* <UseEffect /> */}
         {/* <Timer />
         <UserList /> */}
         {/* <TextIput />
